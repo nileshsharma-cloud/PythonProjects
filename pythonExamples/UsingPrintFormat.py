7f1df3c1a7d3b0e4c6cfbfcb1713bdd74d000ee8
Nilesh Sharma <53362304+nileshsharma-cloud@users.noreply.github.com>
2020-05-21 23:51:50 +0530

    Update code in print format file

--- pythonExamples/UsingPrintFormat.py
+++ pythonExamples/UsingPrintFormat.py
@@ -1,14 +1,24 @@
 import sys
 
+
 def test_pass(did_pass):
     linenum = sys._getframe(1).f_lineno
     if did_pass:
-        msg = "this is at line number {0} ok.",format(linenum)
+        msg = "this is at line number {0} ok.".format(linenum)
     else:
-        msg = "this is at line number {0} failed.", format(linenum)
-        print(msg)
-        
+        msg = "this is at line number {0} failed.".format(linenum)
+    print(msg)
+
+
+def absolute_value(x):
+    if x < 0:
+        return True
+    else:
+        return -x
+
+
 def test_suite():
-    test_pass(abs(-17)==17)
-    
+    test_pass(absolute_value(-17) == 17)
+
+
 test_suite()
